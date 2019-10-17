58eb904a14a4d16809cf4b04e3eb917983869494
Patricia Sayonara <patysayonara28@gmail.com>
2019-10-17 20:09:50 -0300

    Colocando os códigos em apenas um arquivo html: index.html

--- Biblioteca/funcoes.js
+++ Biblioteca/funcoes.js
@@ -1,83 +1,43 @@
 $(document).ready(function() {
     
-    $('form').submit(function() {
-        //arrayBooks.push(new Book(document.getElementById('title').value,document.getElementById('author').value,document.getElementById('pages').value,document.getElementById('status').value));
-        titulos.push(document.getElementById("title"));
-        
-        autores.push(document.getElementById("author"));
-        paginas.push(document.getElementById("pages"));
-      });
+    $("form").submit(function(event) {
+        event.preventDefault();
+        arrayBooks.push(new Book($("#title").val(),$("#author").val(),$("#pages").val(),$("#status").val()));
+        alert(arrayBooks.length);
+        fillTable();
+    });
 
-    
-    $("#title").focusout(function(){
-         titlo = $("#title").val();
-         alert(titlo);
-         t =JSON.stringify( $("#title").value())
-         localStorage.setItem("titulo",t);
-     });
-
-    
-
-    $('#changeStatus').click(function(){
-        if($(this).text() == 'Read'){
-            $(this).html('Unread');
+    $(".changeStatus").click(function(){
+        if($(this).text() == "Read"){
+            $(this).html("Unread");
             //Modificar no objeto
         } else {
-            $(this).html('Read');
+            $(this).html("Read");
             //Modificar no objeto
         }
     });
-
-    fillTable();
     
-    $('#deleteCol').click(function(){
+    $(".deleteCol").click(function(){
         //Modificar no objeto
         deleteBook();
     });
 
-    $('#returnButton').click(function(){
-        window.location.href = 'index.html';
-    });
+    //$('#returnButton').click(function(){
+    //    window.location.href = 'index.html';
+    //});
 
-    $("#atualizar").click(function(){
-        var t = localStorage.getItem("titulo");
-        var n = j + t;
-        alert(n);
-        localStorage.removeItem("titulo");
-       });
-    
-
-     function fillTable(){
-        
-        
-        for(var i = 0; i <titulos.length; i++){
-        
-            $("#rows").append("<td>"+titulos[i]+"</td>");
-            
-            // <td id='titleCol'></td>
-            // <td id='authorCol'></td>
-            // <td id='pagesCol'></td>
-            // <td id='statusCol'><button type='button' id='changeStatus'>Read</button></td>
-            // <td id='deleteCol'><b>Delete</b></td>
-            // document.getElementById('titleCol').value = arrayBooks[i].getTitle();
-            // document.getElementById('authorCol').value = arrayBooks[i].getAuthor();
-            // document.getElementById('pagesCol').value = arrayBooks[i].getPages();
-            // document.getElementById('statusCol').value = arrayBooks[i].getStatus();
+    function fillTable(){
+        for(var i = 0; i <arrayBooks.length; i++){
+            $("tbody").append("<tr>");
+            $("tbody").append("<td>"+arrayBooks[i].getTitle()+"</td>");
+            $("tbody").append("<td>"+arrayBooks[i].getAuthor()+"</td>");
+            $("tbody").append("<td>"+arrayBooks[i].getPages()+"</td>");
+            $("tbody").append("<td><button type='button' class='changeStatus'>"+arrayBooks[i].getStatus()+"</button></td>");
+            $("tbody").append("<td style='color:rgb(172, 0, 0)'><b>Delete</b></td>");
+            $("tbody").append("</tr>");
         }
-    
-    
-    }
-
-      
-
-
+     }
 });
-
-var j ="patricia";
-var titulos = ["a orfa"];
-var autores = ["julia"];
-var paginas = [111];
-var titlo;
 
 var arrayBooks = [];
 
@@ -103,8 +63,6 @@ function Book(title, author, pages, status){
         return this.status;
     }
 }
-
-
 
 function deleteBook(){
     //Encontrar posição do objeto
