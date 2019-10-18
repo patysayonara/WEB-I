1cb1abb488cda117a4abe98fe75a42bb43aeef91
Júlia Ferreira <juliafsouzag@gmail.com>
2019-10-17 23:52:44 -0300

    Remover repetições e alterar layout

--- Biblioteca/funcoes.js
+++ Biblioteca/funcoes.js
@@ -1,70 +1,89 @@
 $(document).ready(function() {
-    
+
     $("form").submit(function(event) {
         event.preventDefault();
-        arrayBooks.push(new Book($("#title").val(),$("#author").val(),$("#pages").val(),$("#status").val()));
-        alert(arrayBooks.length);
+        arrayBooks.push(new Book($("#title").val(), $("#author").val(), $("#pages").val(), $("#status").val()));
         fillTable();
+        // Não funcionam ainda
+        Populate();
+        form.reset();
+
     });
 
-    $(".changeStatus").click(function(){
-        if($(this).text() == "Read"){
-            $(this).html("Unread");
-            //Modificar no objeto
+    // $("button").click(function() {
+    //     if ($(this).text() == "Read") {
+    //         $(this).html("Unread");
+    //     } else {
+    //         $(this).html("Read");
+    //         //Modificar no objeto
+    //     }
+    // });
+
+    $(document).on('click', '.changeStatus', function() {
+        if ($(this).text() == "Read") {
+            $(this).text("Unread");
         } else {
-            $(this).html("Read");
+            $(this).text("Read");
             //Modificar no objeto
         }
     });
-    
-    $(".deleteCol").click(function(){
+
+    $(".deleteCol").click(function() {
         //Modificar no objeto
         deleteBook();
     });
 
-    //$('#returnButton').click(function(){
-    //    window.location.href = 'index.html';
-    //});
 
-    function fillTable(){
-        for(var i = 0; i <arrayBooks.length; i++){
-            $("tbody").append("<tr>");
-            $("tbody").append("<td>"+arrayBooks[i].getTitle()+"</td>");
-            $("tbody").append("<td>"+arrayBooks[i].getAuthor()+"</td>");
-            $("tbody").append("<td>"+arrayBooks[i].getPages()+"</td>");
-            $("tbody").append("<td><button type='button' class='changeStatus'>"+arrayBooks[i].getStatus()+"</button></td>");
-            $("tbody").append("<td style='color:rgb(172, 0, 0)'><b>Delete</b></td>");
-            $("tbody").append("</tr>");
-        }
-     }
+    function fillTable() {
+        $("#title").attr("value", "ola");
+        var i = arrayBooks.length - 1;
+        $("tbody").append("<tr>");
+        $("tbody").append("<td>" + arrayBooks[i].getTitle() + "</td>");
+        $("tbody").append("<td>" + arrayBooks[i].getAuthor() + "</td>");
+        $("tbody").append("<td>" + arrayBooks[i].getPages() + "</td>");
+        $("tbody").append("<td><button type='button' class='changeStatus btn btn-outline-info'>" + arrayBooks[i].getStatus() + "</button></td>");
+        $("tbody").append("<td><button type='button' class='deleteCol btn btn-outline-danger'>Delete</button></td>");
+        $("tbody").append("</tr>");
+
+    }
+
+
 });
 
 var arrayBooks = [];
 
-function Book(title, author, pages, status){
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.status = status;
 
-    this.getTitle = function(){
+    this.getTitle = function() {
         return this.title;
     }
 
-    this.getAuthor = function(){
+    this.getAuthor = function() {
         return this.author;
     }
 
-    this.getPages = function(){
+    this.getPages = function() {
         return this.pages;
     }
 
-    this.getStatus = function(){
+    this.getStatus = function() {
         return this.status;
     }
 }
 
-function deleteBook(){
+function deleteBook() {
     //Encontrar posição do objeto
-    arrayBooks.split(pos,1);
+    arrayBooks.split(pos, 1);
+}
+
+function Populate() {
+    arrayBooks.push(new Book("O Conde de Monte Cristo", " Alexandre Dumas", 300, "read"));
+    arrayBooks.push(new Book("Fahrenheit 451", " Ray Bradbury", 300, "read"));
+    arrayBooks.push(new Book("Madame Bovary", " Gustave Flaubert", 300, "read"));
+    arrayBooks.push(new Book("A Insustentável Leveza do Ser", "Milan Kundera", 300, "read"));
+
 }
